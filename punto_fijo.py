fe4101a1b5029fc3e7b13fd3405656be559b12e9
Nicolas Aguerre <aguerre31@gmail.com>
2019-04-25 15:57:23 -0300

    Agregado el valor del punto medio a la tabla de biseccion

--- punto_fijo.py
+++ punto_fijo.py
@@ -1,6 +1,6 @@
 from Equation import Expression
 import math
-
+import numpy as np
 
 def wrapper_punto_fijo(funcion, derivada, semilla, semilla_b, error, valor_raiz, max_iteraciones):
 	return punto_fijo(funcion, semilla, error, max_iteraciones)
@@ -10,14 +10,14 @@ def wrapper_punto_fijo(funcion, derivada, semilla, semilla_b, error, valor_raiz,
 def punto_fijo(funcion_pf,semilla,error,max_iteraciones = 10000):
 	err = np.longdouble(math.inf)
 	p = np.longdouble(semilla)
-	Fp = funcion(p)
+	Fp = funcion_pf(p)
 
 	i = 0 
 	tabla = [['i','p','F(p)','err_rel','cota_err'],[i,p,Fp,'-','-']]
 	while err > error and i < max_iteraciones:
 		i += 1
 		p = Fp
-		Fp = funcion(p)
+		Fp = funcion_pf(p)
 		err = abs(p - tabla[-1][1])
 		tabla.append([i,p,Fp,err/p,err])
 
